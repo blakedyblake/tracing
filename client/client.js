8c4e206d659327d7c31eb20e4891a44557680ceb
Blake Nuttall <blakedyblake@gmail.com>
2021-08-31 14:07:02 -0600

    Trying to make divs pop up

--- client/client.js
+++ client/client.js
@@ -2,6 +2,10 @@ document.querySelector('form').addEventListener('submit',(event)=>{
     event.preventDefault()
     let newDoc = document.createElement('div')
     let input = document.getElementById('in').innerText;
+    document.getElementById('in').innerText = '';
+
+
     newDoc.innerHTML = `<p>${input}</p>`
+    document.querySelector('body').append(newDoc);
 })
 
