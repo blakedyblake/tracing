bbc3979c37adda94ad8310d2d61d4a80e76242a6
Blake Nuttall <blakedyblake@gmail.com>
2021-08-31 14:44:37 -0600

    random positions

--- client/client.js
+++ client/client.js
@@ -4,6 +4,11 @@ document.querySelector('form').addEventListener('submit',(event)=>{
     let input = document.getElementById('in').value;
     document.getElementById('in').value = '';
 
+    newDoc.style.position = 'absolute';
+    newDoc.style.top = `${15 + 20 *Math.floor(Math.random()*20)}%`
+    newDoc.style.left = `${15 + 10 *Math.floor(Math.random()*20)}%`
+
+
 
     newDoc.innerHTML = `<p>${input}</p>`
     document.querySelector('body').appendChild(newDoc);
