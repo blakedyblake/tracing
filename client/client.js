d5ef2fdd8d196077447bba0d9c4663d166cae48e
Blake Nuttall <blakedyblake@gmail.com>
2021-08-31 14:14:38 -0600

    border divs and empty input

--- client/client.js
+++ client/client.js
@@ -2,7 +2,7 @@ document.querySelector('form').addEventListener('submit',(event)=>{
     event.preventDefault()
     let newDoc = document.createElement('div')
     let input = document.getElementById('in').value;
-    document.getElementById('in').innerText = '';
+    document.getElementById('in').value = '';
 
 
     newDoc.innerHTML = `<p>${input}</p>`
